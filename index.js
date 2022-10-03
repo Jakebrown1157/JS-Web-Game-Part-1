4c5279b536605661075b3a4b3a671c02a5db24f8
jake <jakebrown536@gmail.com>
2022-10-02 19:38:03 -0600

    sup

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-let greenCharacter = document.createElement('img')
-greenCharacter.src = 'assets/green-character.gif'
-greenCharacter.style.position = 'fixed'
-greenCharacter.style.left = '100px'
-greenCharacter.style.bottom = '100px'
-document.body.append(greenCharacter)
+function newImage(x,y,z){
+    let object = document.createElement('img')
+    object.src = x
+    object.style.position = 'fixed'
+    object.style.left = y
+    object.style.bottom = z
+    document.body.append(object)
+    return object
+}
+
+function newItem(x,y,z){
+    let object = newImage(x,y,z)
+
+    object.addEventListener('dblclick', function(){
+        object.remove()
+    })
+}
+
+newImage('assets/green-character.gif','100px','100px')
+newImage('assets/tree.png','200px','300px')
+newImage('assets/pillar.png','350px','100px')
+newImage('assets/pine-tree.png','450px','200px')
+newImage('assets/crate.png','150px','200px')
+newImage('assets/well.png','500px','425px')
+
+newItem('assets/sword.png','500px','405px')
+newItem('assets/shield.png','165px','185px')
+newItem('assets/staff.png','600px','100px')
